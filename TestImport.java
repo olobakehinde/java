c9ca3be905f32787f15226b9d404e729e4dc5409
olobakehinde <olobakehinde@gmail.com>
2022-08-23 10:50:24 +0100

    testarray
    Signed-off-by: olobakehinde <olobakehinde@gmail.com>

---
+++ TestImport.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class TestImport {
+
+    public static void main(String[] args) {
+        
+        String [] shopitem = new String [];
+
+        shopitem [] = {"books","pen","locks"} ;
+
+        for (int i=0; i <=shopitem.length; i++ ){
+
+            System.out.println("shoplist" + shopitem[i]);
+        }
+    }
+    
+}
