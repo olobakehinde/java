0efc2d6a604aebcd7ef5b7bcc68f4e114786dafc
olobakehinde <olobakehinde@gmail.com>
2022-08-23 11:13:40 +0100

    update code
    Signed-off-by: olobakehinde <olobakehinde@gmail.com>

--- TestImport.java
+++ TestImport.java
@@ -1,16 +1,23 @@
+import java.util.Arrays;
 import java.util.Scanner;
 public class TestImport {
 
     public static void main(String[] args) {
         
-        String [] shopitem = new String [];
+        Scanner num = new Scanner(System.in);
 
-        shopitem [] = {"books","pen","locks"} ;
+        System.out.println("total number of item to buy");
+        int itemNum =num.nextInt();
 
-        for (int i=0; i <=shopitem.length; i++ ){
+        String [] shopitem = new String[itemNum];
 
-            System.out.println("shoplist" + shopitem[i]);
+        for (int i=0; i<shopitem.length; i++ ){
+            Scanner item = new Scanner(System.in);
+            System.out.println("enter your item with respect to your itemNum"+" "+itemNum);
+            shopitem[i]=item.next();
         }
+
+        System.out.println(Arrays.toString(shopitem));
     }
     
 }
